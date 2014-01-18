1c1abf81bfeaef41d59cfe4aa87d81bf350d6c02
Michael <michaelyoung1995@gmail.com>
2014-01-18 02:38:32 -0500

    api added

--- WeatherAlarm/src/com/mahcks/weatheralarm/MainActivity.java
+++ WeatherAlarm/src/com/mahcks/weatheralarm/MainActivity.java
@@ -3,6 +3,7 @@ package com.mahcks.weatheralarm;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.os.StrictMode;
 
 /*
  * Main activity
@@ -11,6 +12,14 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+    	
+    	StrictMode.ThreadPolicy policy = new StrictMode.
+    			ThreadPolicy.Builder().permitAll().build();
+    			StrictMode.setThreadPolicy(policy); 
+    	
+    	WeatherGetter.getWeather();
+    	System.out.println("hello world");
+    	
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
---
+++ WeatherAlarm/src/com/mahcks/weatheralarm/WeatherGetter.java
@@ -0,0 +1,63 @@
+package com.mahcks.weatheralarm;
+
+import java.net.*;
+import java.io.*;
+import java.lang.*;
+import org.json.*;
+
+
+
+public class WeatherGetter {
+
+	public static WeatherData getWeather(){
+		try {
+				System.out.println("Getting weather");
+			  URL url = new URL("http://api.wunderground.com/api/8092824ad063a128/conditions/q/US/Michigan/Detroit.json");
+			  HttpURLConnection con = (HttpURLConnection) url
+			    .openConnection();
+			  
+			  System.out.println("STREAM:" +con.getInputStream().toString());
+			  
+			  JSONObject data = new JSONObject(readStream(con.getInputStream()));
+			  
+			  
+			  return new WeatherData((JSONObject)data.get("current_observation"));
+
+			  
+			  } catch (Exception e) {
+			  System.out.println("error");
+			  e.printStackTrace();
+			}
+			return null;
+	}
+	
+	
+	static private String readStream(InputStream in) {
+		  BufferedReader reader = null;
+		  StringBuffer strBuf = new StringBuffer();
+		  try {
+		    reader = new BufferedReader(new InputStreamReader(in));
+		    String line = "";
+		    while ((line = reader.readLine()) != null) {
+		      strBuf.append(line);
+		    }
+		  } catch (IOException e) {
+		    e.printStackTrace();
+		  } finally {
+		    if (reader != null) {
+		      try {
+		        reader.close();
+		      } catch (IOException e) {
+		        e.printStackTrace();
+		        }
+		    }
+		    
+		    if(strBuf.length()>0){
+		    	return strBuf.toString();
+		    }return null;
+		  }
+		}
+		
+	
+	
+}
