f3ef966f32e3b55c771aa0e6d1ba54b5728b0f9f
PeterGok <petergok@hotmail.com>
2014-01-17 22:53:23 -0500

    Master - added random comment

--- WeatherAlarm/src/com/mahcks/weatheralarm/MainActivity.java
+++ WeatherAlarm/src/com/mahcks/weatheralarm/MainActivity.java
@@ -19,7 +19,8 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        //comment
+    	getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
     
