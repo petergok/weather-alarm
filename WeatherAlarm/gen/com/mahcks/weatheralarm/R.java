e580cc6d9c8c4fa132428e526f875229cbb287c5
Michael <michaelyoung1995@gmail.com>
2014-01-18 16:23:16 -0500

    deleted mainactivity

--- WeatherAlarm/gen/com/mahcks/weatheralarm/R.java
+++ WeatherAlarm/gen/com/mahcks/weatheralarm/R.java
@@ -30,12 +30,11 @@ public final class R {
         public static final int alarm_list_background=0x7f020000;
         public static final int alarm_list_background_edit=0x7f020001;
         public static final int background=0x7f020002;
-        public static final int background_edit=0x7f020003;
-        public static final int check_icon=0x7f020004;
-        public static final int ic_action_cancel=0x7f020005;
-        public static final int ic_action_save=0x7f020006;
-        public static final int ic_launcher=0x7f020007;
-        public static final int x_icon=0x7f020008;
+        public static final int check_icon=0x7f020003;
+        public static final int ic_action_cancel=0x7f020004;
+        public static final int ic_action_save=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int x_icon=0x7f020007;
     }
     public static final class id {
         public static final int action_cancel=0x7f0a001e;
