a8126d121d3e03efdc75456297b49cfb91eeae92
Simon <simonhuang989@hotmail.com>
2014-01-19 04:24:18 -0500

    added actual alarm

--- WeatherAlarm/src/com/mahcks/weatheralarm/AlarmRingActivity.java
+++ WeatherAlarm/src/com/mahcks/weatheralarm/AlarmRingActivity.java
@@ -1,18 +1,26 @@
 package com.mahcks.weatheralarm;
 
+import java.util.Calendar;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class AlarmRingActivity extends Activity {
 	
 	String description;
 	int temp;
+	String time;
 	
 	public void onCreate (Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.alarm_alert);
+		
+		Calendar c = Calendar.getInstance();
+		this.time=((int)c.get(Calendar.HOUR_OF_DAY))+":"+((int)c.get(Calendar.MINUTE));
 		
 		WeatherData wd = WeatherGetter.getWeather();
 		this.description = wd.weather;
@@ -24,12 +32,12 @@ public class AlarmRingActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				finish();
+				System.out.println("wat");
 			}
 			
 		});
 		
-		Button snoozeButton = (Button) findViewById(R.id.stopButton);
+		Button snoozeButton = (Button) findViewById(R.id.snoozeButton);
 		snoozeButton.setOnClickListener(new Button.OnClickListener(){
 
 			@Override
@@ -40,21 +48,34 @@ public class AlarmRingActivity extends Activity {
 			
 		});
 		
+		
+		TextView textView = (TextView) findViewById(R.id.temp);
+		textView.setText(this.temp);
+		
+		ImageView imgView = (ImageView)findViewById(R.id.weatherIcon);
+		textView = (TextView) findViewById(R.id.desc);
+		textView.setText(this.description);
 		if (description == wd.SNOW){
-			
+			imgView.setImageResource(R.drawable.snow);
 		}
 		else if (description == wd.RAIN){
-			
+			imgView.setImageResource(R.drawable.rain);
 		}
 		else if (description == wd.CLOUDY){
-			
+			imgView.setImageResource(R.drawable.cloudy);
 		}
 		else if (description == wd.TSTORM){
-			
+			imgView.setImageResource(R.drawable.storm);
 		}
 		else if (description == wd.CLEAR){
-			
+			imgView.setImageResource(R.drawable.sunny);
 		}
+		
+		textView = (TextView) findViewById(R.id.time);
+		textView.setText(this.time);
+		
+
+		WeatherSound.playSound(this, wd.type, true);
 	}
 	
 }
