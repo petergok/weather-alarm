135a63aec2b0a116505d114bd9426f796eea5786
Michael <michaelyoung1995@gmail.com>
2014-01-18 22:27:50 -0500

    Adding alarm schedule

---
+++ WeatherAlarm/src/com/mahcks/weatheralarm/AlarmReceiver.java
@@ -0,0 +1,5 @@
+package com.mahcks.weatheralarm;
+
+public class AlarmReceiver {
+
+}
---
+++ WeatherAlarm/src/com/mahcks/weatheralarm/AlarmScheduler.java
@@ -0,0 +1,39 @@
+package com.mahcks.weatheralarm;
+
+import android.app.*;
+import android.content.*;
+import java.util.Calendar;
+
+
+public class AlarmScheduler {
+
+	public static void setAlarm(Context context,Alarm alarm){
+		
+		AlarmManager alarmMgr = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+		Intent intent = new Intent(context, AlarmReceiver.class);
+		PendingIntent alarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+
+		int hour = Integer.valueOf(alarm.time.substring(0,2));
+		int min = Integer.valueOf(alarm.time.substring(3,5));
+		
+		for(int i=0;i<7;i++){
+			if(alarm.days.charAt(i)=='t'){
+				// Set the alarm to start at 8:30 a.m.
+				Calendar calendar = Calendar.getInstance();
+				calendar.setTimeInMillis(System.currentTimeMillis());
+				calendar.set(Calendar.DAY_OF_WEEK, i);
+				calendar.set(Calendar.HOUR_OF_DAY, hour%12);
+				calendar.set(Calendar.MINUTE, min);
+				if(hour<12)calendar.set(Calendar.AM_PM, Calendar.AM);
+				else calendar.set(Calendar.AM_PM,Calendar.PM);
+		
+				// setRepeating() lets you specify a precise custom interval--in this case,
+				// 20 minutes.
+				alarmMgr.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+				        1000 * 60 * 60 * 24 * 7, alarmIntent);
+			}
+		}
+		
+	}
+	
+}
